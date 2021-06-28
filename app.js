d21c62de62059fde97fce1710ba5c99544eb0f9a
Julian Groen <julian@jgroen.com>
2021-06-28 00:29:50 -0600

    added query string get route

--- app.js
+++ app.js
@@ -12,14 +12,66 @@ const db = mysql.createConnection({
 // Connect to database
 db.connect((err) => {
     if (err) throw err;
-    console.log('MySQL Connected!');
 });
 
 const app = express();
 
+app.set('query parser', 'simple');
+
+//*############//
+//* GET ROUTES //
+//*############//
+
+//Default Route
 app.get(['/', '/api'], (req, res) => {
-    res.send('👽');
+    return res.send('👽');
 });
+
+//Select all rows
+app.get('/api/entries', (req, res) => {
+    let sql = 'SELECT * FROM entries';
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.send([{ error: err.code }]);
+        }
+        return res.send(result);
+    });
+});
+
+// Get entries containing one or more key value pairs via query string
+app.get('/api/entries/q', (req, res) => {
+    const queryParams = Object.entries(req.query);
+    let sqlParams = ``;
+    queryParams.map(([key, value], index) => {
+        const safeKey = key.trim().split(' ', 1);
+
+        // ternary to check if value is an array due to duplicate query string keys
+        const safeValue = Array.isArray(value)
+            ? Number(value[0].trim().split(' ', 1))
+            : Number(value.trim().split(' ', 1));
+
+        sqlParams += `${safeKey} = ${safeValue}`;
+        if (index != queryParams.length - 1) {
+            sqlParams += ` AND `;
+        }
+    });
+    let sql = `SELECT * FROM entries WHERE ${sqlParams};`;
+    console.log(sql);
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.send([{ error: err.code }]);
+        }
+        return res.send(result);
+    });
+});
+
+app.get('/api/hb', (req, res) => {
+    return res.send('❤️🍯🐰');
+});
+
+//*#############//
+//* POST ROUTES //
+//*#############//
 
 // Insert row
 //app.get('/addtest1', (req, res) => {
@@ -31,41 +83,6 @@ app.get(['/', '/api'], (req, res) => {
 //        res.send('Test one was created!');
 //    });
 //});
-
-//Select all rows
-app.get('/api/entries', (req, res) => {
-    let sql = 'SELECT * FROM entries';
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
-    });
-});
-
-//Select specific row by id
-app.get('/api/entries/:id', (req, res) => {
-    let sql = `SELECT * FROM entries WHERE id = "${req.params.id}"`;
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
-    });
-});
-
-//Select rows with specified mood value
-
-app.get('/api/entries/mood/:mood', (req, res) => {
-    let sql = `SELECT * FROM entries WHERE mood = "${req.params.mood}"`;
-    let query = db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
-    });
-});
-
-app.get('/api/hb', (req, res) => {
-    res.send('❤️🍯🐰');
-});
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
