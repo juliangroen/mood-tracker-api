b4eaebb7627b7844a6f7f56c0679cec855c70d16
Julian Groen <julian@jgroen.com>
2021-07-13 22:07:26 -0600

    moved docker files to new branch

--- curl_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#curl -X POST -H "Content-Type: application/json" -d '{"mood": "6", "sleep": "6", "anxiety": "6", "depression": "6", "motivation": "6", "focus": "6"}' localhost:3000/api/entries
-
-#curl -X POST -H "Content-Type: application/json" -d '{"mood": "1", "sleep": "2", "anxiety": "3", "depression": "4", "motivation": "5", "focus": "5"}' localhost:3000/api/entries
-
-#curl -X POST -H "Content-Type: application/json" -d '{"mood": "6", "sleep": "6", "anxiety": "6", "depression": "6", "motivation": "6", "focus": "6"}' localhost:3000/api/entries/7
-
-#curl -X DELETE localhost:3000/api/entries/id/7
